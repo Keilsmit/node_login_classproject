44231e5cd8a01e6628c8654590fd9b555fb05c27
keilsmit <keilsmit@yahoo.com>
2017-08-17 18:13:51 -0400

    revised

--- index.js
+++ index.js
@@ -11,45 +11,47 @@ app.set('views', './views');
 app.set('view engine', 'mustache');
 
 // app.use(express.static('/'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.use(session({
-  secret: "cat",
-  resave: false,
-  saveUninitialized: true
+    secret: "cat",
+    resave: false,
+    saveUninitialized: true
 }));
 
 //Allows us to set up the persistent data
-app.use(function(req, res, next){
-  req.session.users = {
-    keith: "1234567"
-  };
-  next();
+app.use(function(req, res, next) {
+    req.session.users = {
+        keith: "1234567"
+    };
+    next();
 });
 
-app.get('/', function(req, res, next){
-  if (req.session.username){
-    res.send("Hello, " + req.session.username + ". Your password is " + req.session.password + ".");
-  }else{
-    res.redirect("/login")
-  };
-  next();
-});
-
-
-app.get('/login', function(req, res){
-  res.render('index');
+app.get('/', function(req, res, next) {
+    if (req.session.username) {
+        res.send("Hello, " + req.session.username + ". Your password is " + req.session.password + ".");
+    } else {
+        res.redirect("/login")
+    };
+    next();
 });
 
 
-app.post("/login", function(req, res, next){
+app.get('/login', function(req, res) {
+    res.render('index');
+});
 
-  console.log(req.body);
-  console.log(req.session.users[req.body.username]);
+
+app.post("/login", function(req, res, next) {
+
+    console.log(req.body);
+    console.log(req.session.users[req.body.username]);
 
     if (req.session.users[req.body.username] === req.body.password) {
-      req.session.username = req.body.username;
-      req.session.password = req.body.password;
+        req.session.username = req.body.username;
+        req.session.password = req.body.password;
     };
     res.redirect('/')
 });
@@ -60,6 +62,6 @@ app.post("/login", function(req, res, next){
 
 
 
-app.listen(3000, function(){
-  console.log('Your server is running...')
+app.listen(3000, function() {
+    console.log('Your server is running...')
 });
